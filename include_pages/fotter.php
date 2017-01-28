7c5b8c72f0a5013c6da74a4ac203ff731ed85d12
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-01-28 13:52:09 +0100

    footers dampar

--- include_pages/fotter.php
+++ include_pages/fotter.php
@@ -7,36 +7,38 @@
 	* @param author Digitalis
 */
 ?>
-<footer>
-    <div class="col-md-12 full_height nopm">
-        <div class="container-fluid full_height nopm">
-            <div class="row full_height no_margin">
-                <div class="footer_top nopm">
-                    <div class="footer_triangle nopm">
-                        <img class="nopm" src="../Metsense/img/triangel.svg" alt="triangele">
-                    </div>
-                </div>
 
-                <div class="footer_mid">
-                    <div class = "center_vertically_css">
-                        <div class="footer_contact col-md-4 col-md-offset-4 col-xs-12">
-                            <h1>Contact us</h1>
-                            <p> info@metsense.com</p>
-                            <p> +46(0)70 1090230</p>
-                        </div>
+<!--Footer-->
+<footer class="container-fluid footer">
+    <div class="row-fluid footer ">
+        <div class="col-xs-12 footer">
+            <div class="footer_container">
+                <!--styling for the trangle
+                <div class="footer_top col-xs-12">
+                    <div class="footer_triangle col-xs-12">
+                        <img src="../Metsense/img/triangel.svg" alt="triangle">
                     </div>
                 </div>
-
-                <div class = "fotter_low">
-                    <div class="footer_logo_container col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-4 ">
-                        <a href="../Metsense/index.php"><img src="../Metsense/img/logo/logo-w.svg" alt = "Metsense logo"></a>
-                        <div class="footer_info ">
-                            <p>	&copy; 2017 Metsens all rights reserved</p>
-                            <p>Website made by DigitalIS</p>
-                        </div>
+                <!--mid section contact us info
+                <div class="footer_mid col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2">
+                    <div class="footer_contact">
+                        <h1>Contact us</h1>
+                        <p>info@metsense.com</p>
+                        <p>+46(0)70 1090230</p>
                     </div>
                 </div>
+                <!--logo and the creater of the webpage
+                <div class="footer_bot col-xs-12">
+                    <a href="../index.php">
+                        <img class="col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2" src="../Metsense/img/logo/logo.svg" alt="Metsense logo">
+                    </a>
+                    <div class="footer_info col-xs-12">
+                        <p>&copy; 2017 Metsens all rights reserved</p>
+                        <p>Website made by DigitalIS</p>
+                    </div>
+                </div>-->
             </div>
         </div>
     </div>
 </footer>
+
