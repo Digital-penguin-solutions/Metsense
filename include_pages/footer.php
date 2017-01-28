2403b60ef64c8e70df086132b43746aa06cedc3c
dr-fluff <oliver.gustavssons@gmail.com>
2017-01-28 16:06:59 +0100

    footer is working for desktop

--- include_pages/footer.php
+++ include_pages/footer.php
@@ -10,35 +10,35 @@
 
 <!--Footer-->
 <footer class="container-fluid footer">
-    <div class="row-fluid footer ">
-        <div class="col-xs-12 footer">
-            <div class="footer_container">
-                <!--styling for the trangle-->
-                <div class="footer_top col-xs-12">
-                    <div class="footer_triangle col-xs-12">
-                        <img src="../Metsense/img/triangel.svg" alt="triangle">
-                    </div>
-                </div>
-                <!--mid section contact us info
-                <div class="footer_mid col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2">
-                    <div class="footer_contact">
-                        <h1>Contact us</h1>
-                        <p>info@metsense.com</p>
-                        <p>+46(0)70 1090230</p>
-                    </div>
-                </div>
-                <!--logo and the creater of the webpage
-                <div class="footer_bot col-xs-12">
-                    <a href="../index.php">
-                        <img class="col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2" src="../Metsense/img/logo/logo.svg" alt="Metsense logo">
-                    </a>
-                    <div class="footer_info col-xs-12">
-                        <p>&copy; 2017 Metsens all rights reserved</p>
-                        <p>Website made by DigitalIS</p>
-                    </div>
-                </div>-->
-            </div>
-        </div>
-    </div>
+	<div class="row-fluid footer ">
+		<div class="col-xs-12 footer">
+			<div class="footer_container">
+				<!--styling for the trangle-->
+				<div class="footer_top col-xs-12">
+					<div class="footer_triangle col-xs-12">
+						<img src="../Metsense/img/triangel.svg" alt="triangle">
+					</div>
+				</div>
+				<!--mid section contact us info-->
+				<div class="footer_mid col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2">
+					<div class="footer_contact">
+						<h1>Contact us</h1>
+						<p>info@metsense.com</p>
+						<p>+46(0)70 1090230</p>
+					</div>
+				</div>
+				<!--logo and the creater of the webpage-->
+				<div class="footer_bot col-xs-12">
+					<a href="../index.php">
+						<img class="col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2" src="../Metsense/img/logo/logo-w.svg" alt="Metsense logo">
+					</a>
+					<!--ator of webpage and copyright stuff-->
+				</div>
+				<div class="footer_info col-xs-12">
+					<p>&copy; 2017 Metsens all rights reserved</p>
+					<p>Website made by DigitalIS</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </footer>
-
