bde09475d6db608e90ef80da48a5c026b8926518
peduncle <jakob.frykmer@gmail.com>
2017-02-05 23:34:01 +0100

    ad

--- add_product.php
+++ add_product.php
@@ -2,55 +2,56 @@
 <html lang="en">
 
 <head>
-    <!--MÅSTE SKRIVAS EN DESCRIPTION PÅ HEMSIDAN !!!!-->
     <meta name="description" content="MetSense - Complete sensor solutions for winter maintenance.">
-    <title>MetSense add product</title>
+    <title>MetSense-Home</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="Klimator">
+    <meta name="author" content="Digital Internet solutions">
 
+    <!--main css style scheat-->
+    <link rel="stylesheet" href="css/style.css">
 
-        <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900i" rel="stylesheet">
 
-        <!--======================================= Font ===================================-->
-        <!--Font awsome-->
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900i" rel="stylesheet">
-        <!--======================================= /*Font ================================-->
+    <!--======================================= JAVA SCRIPT ===========================-->
+    <!--JQuery-->
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <!--Velocity -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.js" type="text/javascript"></script>
+    <script src="js/jquery.color.js"></script>
 
-        <!--======================================= JAVA SCRIPT ===========================-->
-        <!--JQuery-->
-        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-        <!--Velocity -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.js" type="text/javascript"></script>
-        <script src="../js/jquery.color.js"></script>
+    <!-- JavaScript -->
+    <script src="js/scripts.js"></script>
+    <!-- Smooth scoll-->
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+        smoothScroll.init();
+    </script>
+    <script src="js/jquery.stellar.js"></script>
+    <script>
+        $(document).ready(function(e) {
+            $(window).stellar();
+        });
+    </script>
 
-        <!-- JavaScript -->
-        <script src="../js/admin_script.js"></script>
+    <!--Fade in-->
+    <script src="js/fade.in.js"></script>
+    <!--slide in-->
+    <script src="js/slide.in.js"></script>
 
-        <!-- JavaScript -->
-        <script src="../js/scripts.js"></script>
+    <!-- Latest compiled and minified JavaScript for bootstrap-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-        <!-- Smooth scoll-->
-        <script src="../js/smooth-scroll.min.js"></script>
-        <!--Appear lib -->
-        <script src="../js/jquery.appear.js"></script>
-        <!--Fade in-->
-        <script src="../js/fade.in.js"></script>
-        <!--slide in-->
-        <script src="../js/slide.in.js"></script>
+    <!--======================================= /* JAVA SCRIPT ========================-->
+    <!--icon-->
+    <link rel="SHORTCUT ICON" href="img/logo/icontop.png" type="image/x-icon"/>
 
-
-        <!-- Latest compiled and minified JavaScript for bootstrap-->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-        <!--======================================= /* JAVA SCRIPT ========================-->
-        <!--icon-->
-        <link rel="shortcut icon" href="/site01/img/logo/icontop.png" type="image/x-icon" />
-
-        <!-- Latest compiled and minified JavaScript for bootstrap -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity=		"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>    
-    </head>
+    <!-- Latest compiled and minified JavaScript for bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
 
 <?php
 ini_set('memory_limit', '-1'); // not best but it works
