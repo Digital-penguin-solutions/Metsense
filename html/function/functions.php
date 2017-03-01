88d7e903c653b58f37683534b82c4d6b3edb95b1
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-03-01 21:30:22 +0100

    lite reparering

--- html/function/functions.php
+++ html/function/functions.php
@@ -261,12 +261,9 @@ if(!isset($functions_included)){
         $last = strtolower($val[strlen($val)-1]);
         switch($last) {
             // The 'G' modifier is available since PHP 5.1.0
-            case 'g':
-                $val *= 1024;
-            case 'm':
-                $val *= 1024;
-            case 'k':
-                $val *= 1024;
+            case 'g':$val *= 1024;
+            case 'm':$val *= 1024;
+            case 'k':$val *= 1024;
         }
 
         return $val;
