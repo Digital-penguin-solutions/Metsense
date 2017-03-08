86e9322256a0e38db5f9dc084f05e2aad4cfe44e
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-03-08 23:10:50 +0100

    fixar commitment errors

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp        = require('gulp');
 var less        = require('gulp-less');
 var path        = require('path');
 var cleanCss    = require('gulp-clean-css');
-var prefix      = require('gulp-postcss');
+var prefix      = require('gulp-autoprefixer');
 var uglify      = require('gulp-uglify');
 var concat      = require('gulp-concat');
 var htmlmin     = require('gulp-htmlmin');
@@ -57,14 +57,17 @@ gulp.task('less', function() {
         .pipe(gulp.dest('html/css'));
 });
 
-gulp.task('prefix', function () {
+gulp.task('prefix', ['less'], function () {
     return gulp.src('html/css/app.css')
-        .pipe(prefix())
-        .pipe(gulp.dest('html/css/s'));
+        .pipe(prefix({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('html/css/'));
 });
 
 //minify css files and run the sass function before
-gulp.task('minify-css', ['less'], function() {
+gulp.task('minify-css', ['prefix'], function() {
     return gulp.src('html/css/*.css')
         .pipe(plumber())
         .pipe(cleanCss({compatibility: 'ie8'}))
