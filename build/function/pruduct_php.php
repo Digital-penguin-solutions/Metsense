639ba36570dfbe4bb44350f56e58cdcd632cf8e5
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-04-03 19:39:28 +0200

    fixar bilden på Andreas

---
+++ build/function/pruduct_php.php
@@ -0,0 +1,32 @@
+<?php
+
+include "function/functions.php";
+
+$con = connect();
+
+if (isset($_GET['p'])) {
+    $product_name = $_GET['p'];
+    $product_id = get_product_id_by_name($con, $product_name);
+
+}
+else {
+    header("Location: index.php");
+}
+
+$product = get_product_by_id($con, $product_id);
+
+// texts
+$product_name = $product["name"];
+$product_short_description= $product["short_description"];
+$product_long_description= $product["long_description"];
+$product_price = $product["price"];
+
+// images
+$main_image = $product["main_image"];
+$key_features_image = $product["key_features_image"];
+$about_image = $product["about_image"];
+
+$slider_images = get_product_images_by_id($con, $product_id);
+$key_features = get_key_features_by_id($con, $product_id);
+$tech_table_array = get_tech_table_by_id($con, $product_id);
+?>
