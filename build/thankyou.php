6a67c6576bcd99873fca46fa18bfc34ce266af7c
peduncle <jakob.frykmer@gmail.com>
2017-04-03 19:08:29 +0200

    new

--- build/thankyou.php
+++ build/thankyou.php
@@ -7,8 +7,8 @@
             
             include "function/email_function.php";
             // clears the cart
-            //$_SESSION['cart_ids'] = array();
-            //$_SESSION['cart_num'] = array();
+            $_SESSION['cart_ids'] = array();
+            $_SESSION['cart_num'] = array();
             ?> <?php
         }
     }
@@ -19,4 +19,4 @@ include "function/functions.php";
 ?> <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top "> <?php
 $hide_cart = true;
 include "include_pages/nav.php";
-?> <section id="finish_order" class=""><h1 class="checkout_header">Thank you!</h1><h3 class="admin_header">Your order has been sent to us and we will be in contact with further instructions very soon.<br><br>Please contact <strong>info@metsense.com</strong> if you have any more questions in the meantime.</h3><a href="index" class="blue finish_button col-xs-3 col-xs-offset-5">Home</a></section></body></html>
+?> <section id="finish_order" class=""><h1 class="checkout_header">Thank you!</h1><h3 class="admin_header">Your order has been sent to us and we will be in contact with further instructions very soon.<br><br>Please contact <strong>info2@metsense.com</strong> if you have any more questions in the meantime.</h3><a href="index" class="blue finish_button col-xs-3 col-xs-offset-5">Home</a></section></body></html>
